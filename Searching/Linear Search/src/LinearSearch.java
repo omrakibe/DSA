7d714fb9ad0b2f3f53bb6137d33567ee08a6dd52
omrakibe <rakibeom30@gmail.com>
2025-06-09 13:02:00 +0530

    Revising linear search

--- Searching/Linear Search/src/LinearSearch.java
+++ Searching/Linear Search/src/LinearSearch.java
@@ -2,18 +2,20 @@ import java.util.Scanner;
 
 interface ISearch
 {
-    int linearSearch(int []arr, int target);
+    int linearSearch(int[] arr, int target);
 }
 
 class Search implements ISearch
 {
     int steps = 0;
-    public int linearSearch(int []arr, int target)
+
+    public int linearSearch(int[] arr, int target)
     {
-        for(int i=0; i<arr.length; i++)
+        for (int i = 0; i < arr.length; i++)
         {
             steps++;
-            if(arr[i] == target) {
+            if (arr[i] == target)
+            {
                 System.out.println("Steps: " + steps);
                 return i;
             }
@@ -25,23 +27,24 @@ class Search implements ISearch
 
 public class LinearSearch
 {
-    public static void main(String [] args)
+    public static void main(String[] args)
     {
         Scanner scan = new Scanner(System.in);
         int[] arr = new int[100000];
-        for(int i=0; i<arr.length; i++)
+        for (int i = 0; i < arr.length; i++)
         {
-            arr[i] = (int)Math.floor(Math.random() * 100000);
+            arr[i] = (int) Math.floor(Math.random() * 100000);
         }
 
         System.out.print("Enter Number to search in array: ");
         int target = scan.nextInt();
         Search ser = new Search();
         int res = ser.linearSearch(arr, target);
-        if(res != -1)
+        if (res != -1)
         {
             System.out.println("Index of " + target + " in an Array is: " + res);
-        } else {
+        } else
+        {
             System.out.println("Target not found in Array");
         }
     }
--- Searching/Linear Search/src/LinearSearchRevision.java
+++ Searching/Linear Search/src/LinearSearchRevision.java
@@ -1,3 +1,59 @@
+import java.util.Scanner;
+
+interface ISearchRevision
+{
+    int LinearSearchRevision(int[] arr, int target);
+}
+
+class SearchRevision implements ISearchRevision
+{
+    int steps;
+
+    public int LinearSearchRevision(int[] arr, int target)
+    {
+        for (int i = 0; i < arr.length; i++)
+        {
+            steps++;
+            if (arr[i] == target)
+            {
+                System.out.println("Steps: " + steps);
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
 public class LinearSearchRevision
 {
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+
+
+        System.out.print("Enter size of an Array: ");
+        int size = scan.nextInt();
+
+        System.out.print("Enter number to Search: ");
+        int target = scan.nextInt();
+
+        int[] arr = new int[size];
+
+        for (int i = 0; i < arr.length; i++)
+        {
+            arr[i] = (int) Math.ceil((Math.random()) * 1000);
+        }
+
+        SearchRevision ser = new SearchRevision();
+        int res = ser.LinearSearchRevision(arr, target);
+
+        if (res != -1)
+        {
+            System.out.println("Index of " + target + " is: " + res);
+        } else
+        {
+            System.out.println("Target not found in an Array.");
+        }
+
+    }
 }
